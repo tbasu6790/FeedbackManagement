9713341ace6e3af6343d7a849c395bbd79818d6e
Titli Basu <titlibasu6790@gmail.com>
2025-09-22 16:25:40 +0530

    Fix

--- app.py
+++ app.py
@@ -198,10 +198,5 @@ if __name__ == "__main__":
     open(LOG_FILE, 'a').close()
     import os
     port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=True)
 
-# if __name__ == "__main__":
-#     import os
-#     port = int(os.environ.get("PORT", 5000))
-#     open(LOG_FILE, 'a').close()
-#     app.run(host="0.0.0.0", port=port, debug=False)
